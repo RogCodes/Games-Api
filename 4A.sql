70e067a3659244a94bce60eb25b19c0d9ec19ed3
Randi Maulana Akbar <59035856+rama0809@users.noreply.github.com>
2020-05-09 21:08:35 +0700

    Update 4A.sql

--- 4A.sql
+++ 4A.sql
@@ -1,7 +1,7 @@
-# menampilkan semua data dari tabel film
+-Menampilkan semua data dari tabel film
 1. SELECT * FROM Film
 
-# menampilkan data per genre
+-Menampilkan data per genre
 2. SELECT a.title, a.durasi, b.name,c.name,d.name,a.photo,a.durasi, a.rating
 FROM Film AS a
 LEFT JOIN Genre AS b
@@ -12,7 +12,7 @@ LEFT JOIN Director AS d
 ON a.id_genre = d.id
 ORDER BY b.id
 
-# menampilkan detail film berdasarkan id
+-Menampilkan detail film berdasarkan id
 3. SELECT *
 FROM Film 
 WHERE id = 2
